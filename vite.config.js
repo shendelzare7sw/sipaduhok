a4b93b5dc39840d9c1c947ae38ef9aea735f9adc
Tabah Ujianto <shendelzare7sw@gmail.com>
2026-06-13 00:21:41 +0700

    Refactor siswa LMS latihan show assets

--- vite.config.js
+++ vite.config.js
@@ -113,6 +113,8 @@ export default defineConfig({
                 'resources/css/siswa/lms/mata-pelajaran/forum/show.css',
                 'resources/js/siswa/lms/mata-pelajaran/forum-show.js',
                 'resources/css/siswa/lms/mata-pelajaran/ujian/review.css',
+                'resources/css/siswa/lms/mata-pelajaran/ujian/show-latihan.css',
+                'resources/js/siswa/lms/mata-pelajaran/ujian/show-latihan.js',
                 'resources/css/siswa/lms/mata-pelajaran/tugas/index.css',
                 'resources/css/siswa/lms/mata-pelajaran/tugas/show.css',
                 'resources/css/siswa/lms/meeting/index.css',
