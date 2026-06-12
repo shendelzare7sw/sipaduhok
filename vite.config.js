3f949d18d6fa1a0c41757ed73e9f8ed3c46f8d9f
Tabah Ujianto <shendelzare7sw@gmail.com>
2026-06-12 17:06:44 +0700

    Refactor orang tua snap payment assets

--- vite.config.js
+++ vite.config.js
@@ -323,6 +323,8 @@ export default defineConfig({
                 'resources/js/account/settings.js',
                 'resources/css/orang-tua/dashboard.css',
                 'resources/js/orang-tua/dashboard.js',
+                'resources/css/orang-tua/pembayaran/snap.css',
+                'resources/js/orang-tua/pembayaran/snap.js',
                 'resources/css/orang-tua/presensi/ajukan-izin.css',
                 'resources/css/orang-tua/presensi/edit-izin.css',
                 'resources/css/orang-tua/presensi/index.css',
