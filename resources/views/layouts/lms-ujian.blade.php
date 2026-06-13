82c511968005f6a786a09a7b8401d96ca3f2cb2c
Tabah Ujianto <shendelzare7sw@gmail.com>
2026-06-13 11:25:31 +0700

    Refactor LMS layout assets

--- resources/views/layouts/lms-ujian.blade.php
+++ resources/views/layouts/lms-ujian.blade.php
@@ -7,14 +7,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Ujian') - HOK Learning</title>
 
-    <!-- Bootstrap 5.3 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-    @vite(['resources/css/layouts/lms-ujian.css'])
+    @vite(['resources/css/layouts/lms-ujian.css', 'resources/js/layouts/lms-ujian.js'])
     @stack('styles')
 </head>
 
@@ -45,8 +41,6 @@
         </div>
     </div>
 
-    <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     @stack('scripts')
 </body>
 
